94a40ffac7fdf7e54aec4dea66cef4c43dcd7ac9
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-04-05 23:49:30 -0300

    feat: making the get /soccers/results endpoint accept the league param by league name

--- src/routes/sportsRoutes.js
+++ src/routes/sportsRoutes.js
@@ -3,21 +3,47 @@ const express = require("express");
 const axios = require("axios");
 //TO DO: Fazer as rotas e integrar com a lógica de cache
 const router = express.Router();
+const fetchLeagues = require("../services/leagueCacheService").fetchLeagues;
 
-// Get last 10 soccer match results
 router.get("/soccer/results", async (req, res) => {
   try {
-    const response = await axios.get(`${process.env.API_FOOTBALL_URL}/fixtures?season=2023&league=39`, {
+    const { league: leagueName, season} = req.query;
+
+    if (!leagueName || !season) {
+      return res.status(400).json({
+        error: "Missing required query parameters: 'league' and 'season'.",
+      });
+    }
+
+    
+    const leaguesMap = await fetchLeagues(season);
+    const leagueId = leaguesMap[leagueName.toLowerCase().trim()];
+
+    if (!leagueId) {
+      return res.status(400).json({
+        error: "League not found. Please check the spelling or availability.",
+        provided: leagueName,
+      });
+    }
+
+    
+    const queryParams = new URLSearchParams({
+      season,
+      league: leagueId,
+    });
+
+    const url = `${process.env.API_FOOTBALL_URL}/fixtures?${queryParams.toString()}`;
+
+    const response = await axios.get(url, {
       headers: {
         "x-apisports-key": process.env.API_FOOTBALL_KEY,
       },
     });
 
-    // Send API data back to the client
     res.json(response.data);
   } catch (error) {
-    console.error("Error fetching soccer results:", error);
-    res.status(500).json({ error: "Failed to fetch soccer results" });
+    console.error("Error fetching soccer results:", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
